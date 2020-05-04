051401d2487d9b3de87bdfe3b7949cde6c907550
ZuneraIrshad <62567814+ZuneraIrshad@users.noreply.github.com>
2020-05-04 11:23:35 -0400

    added changes logic in config changes block

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -21,28 +21,26 @@ def call(Map config=[:]){
                     writer.writeLine('/t' + file.name + '/t' + file.length());
                 }
         }
-    }
     def date = new Date()
     def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-    echo "Date and Time IS: " + sdf.format(date)
-   
+    //echo "Date and Time IS: " + sdf.format(date)
+    writer.writeLine("Date and Time IS: " + sdf.format(date));
     // currentBuild.getNumber - this gives me last build number
-    echo "Build Number IS: ${BUILD_NUMBER}";
+    writer.writeLine("Build Number IS: ${BUILD_NUMBER}");
     
     def changeLogSets = currentBuild.changeSets;
     
-    for (change in changeLogSets) {
-        def entries = change.items;
-        for (entry in entries) {
-            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-            for (file in entry.affectedFiles) {
-                echo "  ${file.editType.name} ${file.path}";
-            }
-        }
-    }   
     if (config.changes != "false"){
-        echo "changes";
+        for (change in changeLogSets) {
+            def entries = change.items;
+            for (entry in entries) {
+                writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}")
+                for (file in entry.affectedFiles) {
+                    writer.writeLine("${file.editType.name} ${file.path}");
+                }
+            }
+        }   
     }
 }
- 
+// added changes logic in config changes block
 // map is key/value pair 
