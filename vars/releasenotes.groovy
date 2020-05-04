46589a206e464562ef535bc82358de92b1fd32a0
ZuneraIrshad <62567814+ZuneraIrshad@users.noreply.github.com>
2020-05-04 11:12:11 -0400

    added time/date logic

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -2,6 +2,7 @@ import java.io.*;
 import groovy.io.*;
 import java.util.Calendar.*;
 import java.text.SimpleDateFormat
+import hudson.model.* 
 // place most groovy code in var dir 
 // noncps is for testing - i need to rewrite this script without CPS and Closure
 @NonCPS
@@ -24,8 +25,21 @@ def call(Map config=[:]){
     def date = new Date()
     def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
     echo "Date and Time IS: " + sdf.format(date)
+   
     // currentBuild.getNumber - this gives me last build number
     echo "Build Number IS: ${BUILD_NUMBER}";
+    
+    def changeLogSets = currentBuild.changeSets;
+    
+    for (change in changeLogSets) {
+        def entries = change.items;
+        for (entry in entries) {
+            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+            for (file in entry.affectedFiles) {
+                echo "  ${file.editType.name} ${file.path}";
+            }
+        }
+    }   
     if (config.changes != "false"){
         echo "changes";
     }
